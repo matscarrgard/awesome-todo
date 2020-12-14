df91fe0a067989b22071560e1f2bf63d01254b4c
Carrgard <carrgard@f01898a4fd97.ant.amazon.com>
2020-12-14 08:42:49 +0200

    First Quasar file upload.

---
+++ src/pages/Index.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page padding>
+    <button
+      style="position: absolute; right: 10px"
+      @click="counter++"
+    >
+      {{ counter }}
+    </button>
+    <input 
+      v-model="message" 
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage" />
+
+    <button @click="clearMessage">Clear</button>
+    
+    <h5 
+      v-if="message.length"
+      class="border-grey" >{{ message }}</h5>
+    <h6 v-else>No message entered. :(</h6>
+
+    <hr>
+
+    <p>Uppercase message: {{ messageUppercase }}</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "I love Vue.js!",
+      counter: 0
+    }
+  },
+  computed: {
+      messageUppercase() {
+      console.log('messageUppercase was fired.')
+      return this.message.toUpperCase()
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = ''
+    },
+    alertMessage(){
+      alert(this.message)
+    },
+    
+  }
+}
+</script>
+
+<style>
+  .border-grey {
+    border: 1px solid grey;
+  }
+</style>
